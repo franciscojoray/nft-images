dc6941a39e2dd5e539d8d2bc99f162fe68d5e926
Francisco Joray <franjoray@gmail.com>
2024-01-11 02:08:31 -0300

    returning service response in variable

--- semantic-segmentation/service.py
+++ semantic-segmentation/service.py
@@ -1,17 +1,22 @@
 from snet.sdk import SnetSDK
-import config
 import segmentation_pb2
 import segmentation_pb2_grpc
 
 
 
 def invoke_service():
-   snet_config = {"private_key": config.PRIVATE_KEY, "eth_rpc_endpoint": config.ETH_RPC_ENDPOINT}
+   snet_config = {
+      "private_key": PRIVATE_KEY,
+      "eth_rpc_endpoint": ETH_RPC_ENDPOINT,
+      "free_call_auth_token-bin": FREE_CALL_AUTH_TOKEN_BIN,
+      "free-call-token-expiry-block": FREE_CALL_TOKEN_EXPIRY_BLOCK,
+      "email": EMAIL
+   }
    sdk = SnetSDK(config=snet_config)
    service_client = sdk.create_service_client(
-      org_id=config.ORG_ID,
-      service_id=config.SERVICE_ID,
-      service_stub= segmentation_pb2_grpc.SemanticSegmentationStub # replace service_stub
+      org_id=ORG_ID,
+      service_id=SERVICE_ID,
+      service_stub= segmentation_pb2_grpc.SemanticSegmentationStub
    )
    with open("image.jpg", "rb") as f:
       image_content = f.read()
@@ -21,9 +26,10 @@ def invoke_service():
          content = image_content
       ),
       visualise = True
-   )# replace input_method and arguments
-   response = service_client.service.segment(request) # replace service_method
-   print(f"service invoked successfully :: response :: {response}")
+   )
+   response = service_client.service.segment(request)
+   print(f"service invoked successfully")
+   return response
 
 
-invoke_service() # call invoke service method
+response = invoke_service()
